cdd00a493fc03e89db442be4a3555698a5bab7f5
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-02 23:40:33 +0300

    Update DBConnection.java

--- src/main/java/ru/kpfu/itis/java3/semesterwork1/db/DBConnection.java
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/db/DBConnection.java
@@ -1,5 +1,7 @@
 package ru.kpfu.itis.java3.semesterwork1.db;
 
+import ru.kpfu.itis.java3.semesterwork1.exceptions.DBException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,8 +17,8 @@ public class DBConnection {
         if (INSTANCE == null) {
             try {
                 INSTANCE = DriverManager.getConnection(url + dbName, username, password);
-            } catch (SQLException ignored) {
-                //TODO handle
+            } catch (SQLException ex) {
+                throw new DBException(ex.getMessage());
             }
         }
         return INSTANCE;
