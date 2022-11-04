42c82bd34d5220f480daff76e0b9b00821b75598
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-04 23:27:50 +0300

    add logic to servlet

--- src/main/java/ru/kpfu/itis/java3/semesterwork1/servlets/RegistrationServlet.java
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/servlets/RegistrationServlet.java
@@ -1,27 +1,55 @@
 package ru.kpfu.itis.java3.semesterwork1.servlets;
 
+import ru.kpfu.itis.java3.semesterwork1.db.DBProcessor;
+import ru.kpfu.itis.java3.semesterwork1.entity.User;
+import ru.kpfu.itis.java3.semesterwork1.exceptions.DBException;
+import ru.kpfu.itis.java3.semesterwork1.validators.RegistrationInputValidator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLData;
+import java.sql.SQLException;
+import java.sql.SQLType;
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
+    private RegistrationInputValidator registrationInputValidator;
+    private DBProcessor dbProcessor;
+
+    @Override
+    public void init() throws ServletException {
+        registrationInputValidator = new RegistrationInputValidator();
+        dbProcessor = new DBProcessor((Connection) getServletContext().getAttribute("dbConnection"));
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("").forward(req, resp);
+        getServletContext().getRequestDispatcher("/WEB-INF/jsp/registration.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String login = req.getParameter("login");
+        String username = req.getParameter("username");
         String password = req.getParameter("password");
-        //TODO data model
-
-        
-
+        if (!registrationInputValidator.validate(username, password)) {
+            req.setAttribute("errorText", registrationInputValidator.getMessage());
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, resp);
+            return;
+        }
+        if (dbProcessor.containsUser(username)) {
+            req.setAttribute("errorText", "username must be unique");
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, resp);
+            return;
+        }
+        dbProcessor.addUser(username, password);
+        User user = dbProcessor.getUserByUsername(username);
+        req.getSession().setAttribute("userId", user.getId());
+        req.getSession().setAttribute("role", user.getRole());
+        resp.sendRedirect(getServletContext().getContextPath() +  "/profile");
     }
 }
