a89245bfa89856cc8807a46593d32b35356cbdec
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-04 22:27:27 +0300

    Update DBException.java

--- src/main/java/ru/kpfu/itis/java3/semesterwork1/exceptions/DBException.java
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/exceptions/DBException.java
@@ -1,12 +1,21 @@
 package ru.kpfu.itis.java3.semesterwork1.exceptions;
 
+import java.sql.SQLException;
+
 public class DBException extends RuntimeException {
-    // TODO upgrade message
     public DBException() {
         super();
     }
 
     public DBException(String message) {
         super(message);
+    }
+
+    public DBException(SQLException ex) {
+        this(new StringBuffer(ex.getMessage())
+                .append(" db state:")
+                .append(ex.getSQLState())
+                .append(" error code:")
+                .append(ex.getErrorCode()).toString());
     }
 }
