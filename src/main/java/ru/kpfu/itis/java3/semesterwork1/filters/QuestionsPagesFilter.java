57b797ac72b87fe0de11497f54f11d78cc1c003c
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-05 22:39:46 +0300

    Create QuestionsPagesFilter.java

---
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/filters/QuestionsPagesFilter.java
@@ -0,0 +1,19 @@
+package ru.kpfu.itis.java3.semesterwork1.filters;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/questions/*")
+public class QuestionsPagesFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        // TODO implement
+        chain.doFilter(req, res);
+    }
+}
