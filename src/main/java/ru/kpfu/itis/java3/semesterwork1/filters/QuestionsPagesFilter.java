fe68748f507cf320b1222f68da58ff91dc15a64a
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-06 18:58:07 +0300

    implemented filter for questions pages

--- src/main/java/ru/kpfu/itis/java3/semesterwork1/filters/QuestionsPagesFilter.java
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/filters/QuestionsPagesFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("/questions/*")
@@ -13,7 +14,14 @@ public class QuestionsPagesFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        // TODO implement
+
+        HttpSession session = req.getSession();
+        if (session.getAttribute("userId") == null) {
+            req.setAttribute("errorText", "Questions is not available for unauthorized users");
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
+            return;
+        }
+
         chain.doFilter(req, res);
     }
 }
