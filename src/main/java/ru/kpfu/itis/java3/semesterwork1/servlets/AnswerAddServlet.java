05188966a684d1eb6c1dc407061d3fc071cd5ee6
Shamil Vergasov <SRVergasov@stud.kpfu.ru>
2022-11-07 23:32:12 +0300

    Create AnswerAddServlet.java

---
+++ src/main/java/ru/kpfu/itis/java3/semesterwork1/servlets/AnswerAddServlet.java
@@ -0,0 +1,37 @@
+package ru.kpfu.itis.java3.semesterwork1.servlets;
+
+import ru.kpfu.itis.java3.semesterwork1.db.DBProcessor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet("/questions/add_answer")
+public class AnswerAddServlet extends HttpServlet {
+    private DBProcessor dbProcessor;
+
+    @Override
+    public void init() throws ServletException {
+        dbProcessor = new DBProcessor((Connection) getServletContext().getAttribute("dbConnection"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO logic add answer
+        int questionId = Integer.parseInt(req.getParameter("questionId"));
+        int userId = Integer.parseInt(req.getParameter("userId"));
+        String text = req.getParameter("text");
+        System.out.println("" + questionId + " " + userId + " " + text);
+        if (text.isEmpty()) {
+            req.setAttribute("errorText", "Empty text field");
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, resp);
+            return;
+        }
+        dbProcessor.addAnswer(questionId, userId, text);
+        resp.sendRedirect(getServletContext().getContextPath() + "/questions/question?id=" + questionId);
+    }
+}
